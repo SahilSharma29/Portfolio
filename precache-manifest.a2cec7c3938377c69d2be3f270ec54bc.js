b506e0d697b28ff9eb3a164394ae5ecd6e77ca6b
Sahil Sharma <--global>
2024-01-11 18:29:21 +0530

    Updates

---
+++ precache-manifest.a2cec7c3938377c69d2be3f270ec54bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "481d574815be3b030d87",
+    "url": "/static/css/main.405be807.chunk.css"
+  },
+  {
+    "revision": "481d574815be3b030d87",
+    "url": "/static/js/main.481d5748.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1dc3d4bc570b5cae505f2eb5864ffabb",
+    "url": "/index.html"
+  }
+];
